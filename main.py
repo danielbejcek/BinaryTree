d922bfb25b63df41afc81901782b4380b8d03aa3
danielbejcek <daniel.bejcek@seznam.cz>
2024-02-12 14:06:19 +0100

    Added traversal options

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import random
 
 class Node:
     def __init__(self, data):
@@ -9,9 +8,7 @@ class Node:
     def insertNode(self, data):
         if self.data == None:
             self.data = data
-
         else:
-
             if data < self.data:
                 if self.left == None:
                     self.left = Node(data)
@@ -23,17 +20,43 @@ class Node:
                 else:
                     self.right.insertNode(data)
 
+    """Method that prints values in ascending order."""
     def inOrderTraversal(self):
         if self.left:
             self.left.inOrderTraversal()
+        """In case self.left is None (there is no left child of the parent node) we visit (print) the value."""
         print(self.data, end=" ")
         if self.right:
             self.right.inOrderTraversal()
 
+    def preOrderTraversal(self):
+        print(self.data, end=" ")
+        if self.left:
+            self.left.preOrderTraversal()
+        if self.right:
+            self.right.preOrderTraversal()
+
+    def postOrderTraversal(self):
+        if self.left:
+            self.left.postOrderTraversal()
+        if self.right:
+            self.right.postOrderTraversal()
+        print(self.data, end=" ")
+
 root = Node(8)
+root.insertNode(9)
 root.insertNode(4)
 root.insertNode(3)
 root.insertNode(6)
 root.insertNode(2)
+root.insertNode(1)
+root.insertNode(8)
+root.insertNode(10)
+
 root.inOrderTraversal()
+print("\n")
+root.preOrderTraversal()
+print("\n")
+root.postOrderTraversal()
+
 
